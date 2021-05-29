7e9ef7c89906132db313c55d323727f0c4750ea2
mg1924 <mgesullo@gmail.com>
2021-05-29 13:11:56 -0600

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+// If not in production, make sure to use the environment stuff using dotenv
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+// import routes (controllers in MVC terminology)
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose!'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
